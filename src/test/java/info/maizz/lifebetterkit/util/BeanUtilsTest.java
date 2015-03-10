7b99f28d7791bc16b60e82cbd8d197bddf837917
Lucas <stick.goal@163.com>
2015-03-10 10:14:20 +0800

    修改路径

---
+++ src/test/java/info/maizz/lifebetterkit/util/BeanUtilsTest.java
@@ -0,0 +1,38 @@
+package info.maizz.lifebetterkit.util;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class BeanUtilsTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public BeanUtilsTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( BeanUtilsTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
